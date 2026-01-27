38c93c7301ca3d5f0cd03a35064c1413179299c8
CuberLuo <1501858187@qq.com>
2026-01-27 14:57:34 +0800

    update

---
+++ 94.二叉树的中序遍历.js
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=94 lang=javascript
+ *
+ * [94] 二叉树的中序遍历
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function (root) {
+  const arr = []
+  const inorder = (root) => {
+    if (root === null) return
+    inorder(root.left)
+    arr.push(root.val)
+    inorder(root.right)
+  }
+  inorder(root)
+  return arr
+};
+// @lc code=end
+
