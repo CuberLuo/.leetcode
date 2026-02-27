42930e59a721ec02419b48affb9ecf12ae008e86
CuberLuo <1501858187@qq.com>
2026-02-27 22:50:56 +0800

    update

---
+++ top-100-liked/17技巧/136.只出现一次的数字.js
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode.cn id=136 lang=javascript
+ *
+ * [136] 只出现一次的数字
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function (nums) {
+  for (let i = 1; i < nums.length; i++) {
+    nums[0] = nums[0] ^ nums[i]
+  }
+  return nums[0]
+};
+// @lc code=end
+
