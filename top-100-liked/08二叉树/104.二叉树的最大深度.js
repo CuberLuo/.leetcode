9a8e9da9b1c62e20c74229337e4501eab84da1f6
CuberLuo <1501858187@qq.com>
2026-03-02 15:18:12 +0800

    update

---
+++ top-100-liked/08二叉树/104.二叉树的最大深度.js
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode.cn id=104 lang=javascript
+ *
+ * [104] 二叉树的最大深度
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function (root) {
+  if (root === null) return 0
+  const leftDepth = maxDepth(root.left)
+  const rightDepth = maxDepth(root.right)
+  return 1 + Math.max(leftDepth, rightDepth)
+};
+// @lc code=end
+
