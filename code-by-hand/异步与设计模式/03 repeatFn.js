4f7e9be88b253ddac99b8cc939a9dec1ef12ccd4
CuberLuo <1501858187@qq.com>
2026-04-21 14:16:25 +0800

    repeatFn

---
+++ code-by-hand/异步与设计模式/03 repeatFn.js
@@ -0,0 +1,27 @@
+/**
+ * 重复执行函数，间隔 wait 毫秒，共执行 times 次
+ * @param {Function} func - 要执行的函数
+ * @param {number} times - 执行次数
+ * @param {number} wait - 间隔时间(ms)
+ * @returns {Promise}
+ */
+function repeatFn(func, times, wait) {
+  function execute(times) {
+    if (times <= 0) return
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        func()
+        resolve()
+      }, wait)
+    }).then(() => execute(times - 1))
+  }
+  execute(times)
+}
+
+repeatFn(
+  () => {
+    console.log('hello world')
+  },
+  5,
+  3000,
+)
