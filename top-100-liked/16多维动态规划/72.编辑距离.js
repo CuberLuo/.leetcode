fe0d084570051df386739065c89271714b4f6d29
CuberLuo <1501858187@qq.com>
2026-05-07 13:55:05 +0800

    update

---
+++ top-100-liked/16多维动态规划/72.编辑距离.js
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode.cn id=72 lang=javascript
+ *
+ * [72] 编辑距离
+ */
+
+// @lc code=start
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {number}
+ */
+var minDistance = function (word1, word2) {
+  const m = word1.length
+  const n = word2.length
+  // dp[i][j]表示word1前i个字符变成word2前j个字符的最少操作数
+  const dp = new Array(m + 1).fill(0).map(() => new Array(n + 1).fill(0))
+  for (let i = 1; i <= m; i++) dp[i][0] = i //删除i次
+  for (let j = 1; j <= n; j++) dp[0][j] = j //插入j次
+
+  for (let i = 1; i <= m; i++) {
+    for (let j = 1; j <= n; j++) {
+      // word1的第i个字符 = word2的第j个字符
+      if (word1[i - 1] === word2[j - 1]) {
+        dp[i][j] = dp[i - 1][j - 1]
+      } else {
+        dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]) + 1
+      }
+    }
+  }
+  return dp[m][n]
+}
+// @lc code=end
