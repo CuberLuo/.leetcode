222582a5da3e69d9db10994c492e7e428c53304f
CuberLuo <1501858187@qq.com>
2026-05-13 13:57:43 +0800

    update

---
+++ top-interview-150/120.三角形最小路径和.js
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode.cn id=120 lang=javascript
+ *
+ * [120] 三角形最小路径和
+ */
+
+// @lc code=start
+/**
+ * @param {number[][]} triangle
+ * @return {number}
+ */
+var minimumTotal = function (triangle) {
+  const n = triangle.length
+  const dp = new Array(n).fill(0).map(() => new Array(n).fill(0))
+  for (let j = 0; j < triangle[n - 1].length; j++) {
+    dp[n - 1][j] = triangle[n - 1][j]
+  }
+  for (let i = n - 2; i >= 0; i--) {
+    for (let j = 0; j < triangle[i].length; j++) {
+      dp[i][j] = triangle[i][j] + Math.min(dp[i + 1][j], dp[i + 1][j + 1])
+    }
+  }
+  return dp[0][0]
+}
+// @lc code=end
+// prettier-ignore
+const triangle = 
+[
+  [2],
+  [3,4],
+  [6,5,7],
+  [4,1,8,3]
+]
+
+console.log(minimumTotal(triangle))
